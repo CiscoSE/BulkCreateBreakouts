ef50b8122e17be9b1d40012a24003578184edb41
srehling <oldguard@msn.com>
2019-02-08 11:53:59 -0500

    Fully Operational - RC 1

--- CreateInterfaces.sh
+++ CreateInterfaces.sh
@@ -15,13 +15,13 @@
 
 apic='10.82.6.165'
 userName='admin'
-aep=''
-breakoutPortGroup='4x10-Breakout'
-breakoutType='10g-4x'
+AepDN='uni/infra/attentp-L2ColumbiaLab'	# This must be the DN of an existing AEP. I don't check to see if it is there.
+breakoutPortGroup='4x10-Breakout'	# This gets used for a lot of created object names
+breakoutType='10g-4x'			# This is a constant that must not change It must be 10g-4x or 25g-4x
 #breakoutType='25g-4x'
-interfaceProfile='201'
-startInterface=1
-lastInterface=14
+interfaceProfile='201'			# Policy Groups and Profiles are named with this value.
+startInterface=1			# First interface in a range to configure
+lastInterface=14			# Last interface in a range to configure
 
 #Color Coding for screen output.
 green="\e[1;32m"
@@ -42,6 +42,14 @@ function accessAPIC () {
   if [ "$errorCode" != '' ]; then
     writeStatus "APIC Call Failed.\nError Code: ${errorCode}\nXML Result: ${XMLResult}\nType: ${1}\nURL: ${2}\nXML: \n${3}" 'FAIL'
   fi
+  #used only for debuging
+  if [ "${4}" = 'TRUE' ]; then
+	echo "Type: ${1}"
+	echo "URL: ${2}"
+	echo "XML Sent:\n${3}\n\n"
+	echo "XML Result:\n${XMLResult}\n\n"
+	exit
+  fi
 }
 
 
@@ -52,6 +60,8 @@ function getCookie () {
 	echo -n Enter the password for the APIC.
 	read -s password
 	cookieResult=$(curl -sk https://${apic}/api/aaaLogin.xml -d "<aaaUser name='${userName}' pwd='${password}'/>" -c cookie.txt)
+	printf "\n"
+	writeStatus "%5s Cookie Obtained - Access to APIC established"
 }
 
 function writeStatus (){	
@@ -95,15 +105,14 @@ function addPortToBreakout () {
 				annotation='' 
 				descr='' 
 				fromCard='1' 
-				fromPort='$1'
-				name='$interfaceProfile-S1-P$1' 
+				fromPort='${1}'
+				name='${interfaceProfile}-S1-P${1}' 
 				nameAlias='' 
 				toCard='1' 
-				toPort='$1'/>
+				toPort='${1}'/>
 		</infraHPortS>
 	</infraAccPortP> 
   "
-  #local portBreakoutResult=$(curl -b cookie.txt -sk https://${apic}/api/node/mo/uni/infra.xml -d "${portBreakoutXML}" --header "content-type: appliation/xml, accept: application/xml")
   accessAPIC 'POST' "https://${apic}/api/node/mo/uni/infra.xml" "${portBreakoutXML}"
   writeStatus "%10s Breakout Created Successfully"
 }
@@ -120,44 +129,88 @@ function configureBreakoutInterface () {
 		port=$interface
 	fi
 	local name="${interfaceProfile}-P1.${port}.${i}"
-	#TODO where do we get teh AEP DN from?
+	local policyGroupDN="uni/infra/funcprof/accbundle-${name}"
 
 	intPolGrpXML="
 	<infraFuncP>
 	<infraAccBndlGrp 
-	annotation='' 
-	descr='' 
-	dn='uni/infra/funcprof/accbundle-${name}'
+	dn='${policyGroupDN}'
 	lagT='node' 
-	name='${name}' 
-	nameAlias='' 
-	ownerKey='' 
-	ownerTag=''>
+	name='${name}'>
 	<infraRsAttEntP 
 		annotation='' 
-		tDn='uni/infra/attentp-L2ColumbiaLab'/>
+		tDn='${AepDN}'/>
 	<infraRsCdpIfPol 
 		annotation='' 
-		tnCdpIfPolName='CDPEnabled'/>
+		tnCdpIfPolName='CDP-Enabled'/>
 	<infraRsHIfPol 
 		annotation='' 
 		tnFabricHIfPolName='10G'/>
 	<infraRsLacpPol 
 		annotation='' 
-		tnLacpLagPolName='LACPActive'/>
+		tnLacpLagPolName='LACP-Active'/>
 	<infraRsLldpIfPol 
 		annotation='' 
-		tnLldpIfPolName='LLDPEnabled'/>
+		tnLldpIfPolName='LLDP-Enabled'/>
 	</infraAccBndlGrp>
 	</infraFuncP>
 	"      
 	accessAPIC 'POST' " https://${apic}/api/node/mo/uni/infra.xml" "${intPolGrpXML}"
 	writeStatus "%15s interface Policy Group Configured"
-
+	intProfXML="
+	<infraHPortS 
+		dn='uni/infra/accportprof-${interfaceProfile}/hports-${name}-typ-range' 
+		name='${name}' 
+		type='range'>
+		<infraSubPortBlk 
+			fromCard='1' 
+			fromPort='${interface}' 
+			fromSubPort='${i}' 
+			name='${name}' 
+			toCard='1' 
+			toPort='${interface}' 
+			toSubPort='${i}'/>
+			<infraRsAccBaseGrp 
+				tDn='${policyGroupDN}'/>
+	</infraHPortS>
+	"
+	accessAPIC 'POST' "https://${apic}/api/node/mo/uni/infra/accportprof-${interfaceProfile}.xml" "${intProfXML}" 
+	writeStatus "%15s Interface Port Selector Configured for VPC"
     done 
 }
 
+function createDefaultPolicies () {
+  writeStatus "%5s Creating dependency requirements"
+  defaultPolicyXML="
+  <polUni>
+  	<infraInfra>
+		<fabricHIfPol autoNeg='on' descr='' dn='uni/infra/hintfpol-10G'  fecMode='inherit' linkDebounce='100' name='10G'  nameAlias='' ownerKey='' ownerTag='' speed='10G'/>
+		<fabricHIfPol autoNeg='on' descr='' dn='uni/infra/hintfpol-100G' fecMode='inherit' linkDebounce='100' name='100G' nameAlias='' ownerKey='' ownerTag='' speed='100G'/>	
+		<fabricHIfPol autoNeg='on' descr='' dn='uni/infra/hintfpol-40G'  fecMode='inherit' linkDebounce='100' name='40G'  nameAlias='' ownerKey='' ownerTag='' speed='40G'/>	
+		<cdpIfPol adminSt='enabled'  descr='' dn='uni/infra/cdpIfP-CDP-Enabled'  name='CDP-Enabled'  nameAlias='' ownerKey='' ownerTag=''/>
+		<cdpIfPol adminSt='disabled' descr='' dn='uni/infra/cdpIfP-CDP-Disabled' name='CDP-Disabled' nameAlias='' ownerKey='' ownerTag=''/>
+		<lldpIfPol adminRxSt='disabled' adminTxSt='disabled' descr='' dn='uni/infra/lldpIfP-LLDP-Disabled' name='LLDP-Disabled' nameAlias='' ownerKey='' ownerTag=''/>	
+		<lldpIfPol adminRxSt='enabled'  adminTxSt='enabled'  descr='' dn='uni/infra/lldpIfP-LLDP-Enabled'  name='LLDP-Enabled'  nameAlias='' ownerKey='' ownerTag=''/>
+		<lacpLagPol ctrl='fast-sel-hot-stdby,graceful-conv,susp-individual' dn='uni/infra/lacplagp-LACP-Active' maxLinks='16' minLinks='1' mode='active' name='LACP-Active' nameAlias='' ownerKey='' ownerTag=''/>
+		<mcpInstPol adminSt='enabled' dn='uni/infra/mcpInstP-default' initDelayTime='180' loopDetectMult='3' loopProtectAct='port-disable' name='default' ownerKey='DefaultOwnerKey' txFreq='2'/>
+		<edrErrDisRecoverPol dn='uni/infra/edrErrDisRecoverPol-default' errDisRecovIntvl='300' name='default'>
+			<edrEventP descr='' event='event-mcp-loop'  name='' nameAlias='' recover='yes'/>
+			<edrEventP descr='' event='event-bpduguard' name='' nameAlias='' recover='yes'/>
+			<edrEventP descr='' event='event-ep-move'   name='' nameAlias='' recover='yes'/>
+
+		</edrErrDisRecoverPol>
+		
+	</infraInfra>
+  </polUni>
+  "
+  accessAPIC 'POST' "https://${apic}/api/node/mo.xml" "${defaultPolicyXML}"
+}
+
 getCookie
+
+createDefaultPolicies
+
+writeStatus "Ensure a breakout policy exists."
 
 breakoutPolicyXML="
   <infraBrkoutPortGrp
@@ -175,18 +228,14 @@ breakoutPolicyXML="
   </infraBrkoutPortGrp>
 "
 
-writeStatus "\nEnsure a breakout policy exists.\n"
 accessAPIC 'POST' "https://${apic}/api/node/mo/uni/infra/funcprof.xml" "${breakoutPolicyXML}"
-#curl -b cookie.txt -kX POST https://${apic}/api/node/mo/uni/infra/funcprof.xml -d "${breakoutPolicyXML}" --header "content-type: appliation/xml, accept: application/xml" -v
 
 writeStatus "Start loop through each interface"
 
-#TODO Write Default LLDP, CDP, LACP and Link Level policies, or require them to be manually populated by user.  
 for ((interface=1; interface <= lastInterface; interface++))
   do
     addPortToBreakout $interface
     configureBreakoutInterface
-    #TODO Create VPC Policy Group for each breakout interace
     #TODO Create Interface selector for each breakout interface and associate with VPC Policy Group
   done
 
